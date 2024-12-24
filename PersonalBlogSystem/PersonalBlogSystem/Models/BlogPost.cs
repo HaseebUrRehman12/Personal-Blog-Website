88712847e2cb912bd2c3b05adcbbc15f9c91436f
Haseeb Ur Rehman <haseebanis12345@gmail.com>
2024-12-24 10:56:18 +0500

    Personal Blog System uploaded

---
+++ PersonalBlogSystem/PersonalBlogSystem/Models/BlogPost.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PersonalBlogSystem.Models
+{
+    public class BlogPost
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [Column("Title", TypeName ="varchar(255)")]
+        public string Title { get; set; }
+        [Required]
+        public string Content { get; set; }
+        [Required]
+        public DateTime DateCreated { get; set; }
+    }
+
+}
